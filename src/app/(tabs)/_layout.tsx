dc3a2ca79e600fc05ef89903cfecebed114c06ad
Raunak <raunakb22@gmail.com>
2026-07-06 22:11:22 -0400

    pre-routing

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -32,7 +32,7 @@ export default function TabLayout() {
         }}
       />
       <Tabs.Screen
-        name="FoodItemPage"
+        name="test"
         options={{
           title: "Test",
           tabBarIcon: ({ color, size }) => (
